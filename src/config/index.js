6b0587f25d6ebefb334366d430310ebb90f4c3ff
cohencoo <cohencoombs@outlook.com>
2025-06-02 18:28:21 +1000

    update port number

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const config = {
-    port: process.env.PORT || 443,
+    port: process.env.PORT || 80,
     mongoUri: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@visioneerlist.${process.env.DB_KEY}.mongodb.net/?retryWrites=true&w=majority`,
     nodeEnv: process.env.NODE_ENV || "development",
 }
